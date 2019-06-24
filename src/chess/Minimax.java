c115c5e96c0c5c856a2a0fe36f8e03a70700e925
trent.wyatt <trent@trentwyatt.com>
2019-06-23 21:04:13 -0500

    Optimized code.  Removed unnecessary synchronizing.

--- src/chess/Minimax.java
+++ src/chess/Minimax.java
@@ -5,20 +5,12 @@ import java.util.List;
 
 /**
  *
- * Implementation of the Minimax algorithm
+ * Implementation of the Minimax algorithm with alpha-beta pruning
  *
  * Minimax algorithm overview: https://en.wikipedia.org/wiki/Minimax
  *
- * Example code: https://github.com/ykaragol/checkersmaster/blob/master/CheckersMaster/src/checkers/algorithm/MinimaxAlgorithm.java
- *
- * Example chess minimax explanation video: https://www.youtube.com/watch?v=ILI31U3UMI4&list=PLOJzCFLZdG4zk5d-1_ah2B4kqZSeIlWtt&index=46
- *
- * DONE: Added alpha-beta pruning
- *
  */
 public class Minimax implements AIMoveSelector {
-    private final Object alphaLock = new Object();
-    private final Object betaLock = new Object();
     private final Object processedLock = new Object();
     private BoardEvaluator evaluator;
     private Integer movesProcessed;
@@ -30,21 +22,20 @@ public class Minimax implements AIMoveSelector {
         this.maxSeconds = maxSeconds;
         this.startDepth = depth;
         this.movesProcessed = 0;
-        this.stopNanos = 0L;
 
+        // instantiate a BoardEvaluator object to get an identity function for the board state
 //        evaluator = new TotalMoveCounts();
-//        evaluator = new TotalPieceValues();
-        evaluator = new PieceValuesPlusPos();
+//        evaluator = new PieceValuesPlusPos();
+        evaluator = new TotalPieceValues();
     }
 
     @Override
     public int getNumMovesExamined() {
-        int result;
         synchronized (processedLock) {
-            result = movesProcessed;
+            int result = movesProcessed;
             movesProcessed = 0;
+            return result;
         }
-        return result;
     }
 
     /**
@@ -58,16 +49,17 @@ public class Minimax implements AIMoveSelector {
     public Move bestMove(final Board board) {
         int lowest = Integer.MAX_VALUE;
         int highest = Integer.MIN_VALUE;
-        int alpha = Integer.MIN_VALUE;
-        int beta = Integer.MAX_VALUE;
         final int side = board.getTurn();
         int depth = startDepth;
         stopNanos = System.nanoTime() + (maxSeconds * 1_000_000_000L);
         Move bestMove = null;
 
         if (board.getCurrentPlayerMoves().size() == 1) {
-            // We have only one move.  Nothing the other side can do in response will change
-            // what our best move is so just return it now and save the depth cost.
+            // We have only one move so nothing the other side can do in response will change
+            // what move we return so just return it now and save the recursive depth cost.
+            synchronized (processedLock) {
+                ++movesProcessed;
+            }
             return board.getCurrentPlayerMoves().get(0);
         }
 
@@ -94,10 +86,11 @@ public class Minimax implements AIMoveSelector {
                 break;
             }
 
+            boolean maximizing = side != Side.Black;
+
             Thread t = new Thread(() -> {
-                int currentValue = (side == Side.White) ?
-                        this.min(currentBoard, alpha, beta, depth - 1) :
-                        this.max(currentBoard, alpha, beta, depth - 1);
+                int currentValue = minmax(
+                    currentBoard, Integer.MIN_VALUE, Integer.MAX_VALUE, depth - 1, maximizing);
 
                 synchronized (resultsLock) {
                     results.add(currentValue);
@@ -134,43 +127,16 @@ public class Minimax implements AIMoveSelector {
             }
         }
 
-        if (board.getVerbose() >= 4) {
-            System.out.print(" ".repeat((6 - depth) * 2));
-            System.out.print(String.format(
-                    "bestMove(): %s's side, Depth=%d, lowest = %d, highest = %d, best move = ",
-                    side == 1 ? "White" : "Black",
-                    depth,
-                    lowest,
-                    highest
-            ));
-            if (bestMove == null) {
-                System.out.println("null");
-            } else {
-                System.out.println(String.format("%d,%d to %d, %d",
-                        bestMove.getFromCol(),
-                        bestMove.getFromRow(),
-                        bestMove.getToCol(),
-                        bestMove.getToRow()));
-            }
-        }
         return bestMove;
     }
 
-    private int min(final Board board, int alpha, int beta, final int depth) {
+    private int minmax(final Board board, int alpha, int beta, final int depth, final boolean maximizing) {
 
         if (depth <= 0) {
-            int value = evaluator.evaluate(board);
-            if (board.getVerbose() >= 4) {
-                System.out.print(" ".repeat((6 - depth) * 2));
-                System.out.println(String.format(
-                        "min(): %s's turn, Depth=%d, evaluate = %d",
-                        board.getTurn() == 0 ? "White" : "Black",
-                        depth,
-                        value));
-            }
-            return value;
+            return evaluator.evaluate(board);
         }
-        int lowest = Integer.MAX_VALUE;
+
+        int lowestOrHighest = maximizing ? Integer.MIN_VALUE : Integer.MAX_VALUE;
         for (final Move m:board.getCurrentPlayerMoves()) {
             Board currentBoard = new Board(board);
             currentBoard.executeMove(m);
@@ -182,9 +148,13 @@ public class Minimax implements AIMoveSelector {
 
             Thread.yield();
 
-            // See if that move leaves the other player with no moves and return the lowest value possible if so:
+            // See if the move we just made leaves the other player with
+            // no moves and return the best value possible if so:
             if (currentBoard.getCurrentPlayerMoves().size() == 0) {
-                lowest = Integer.MIN_VALUE + (100 - depth);
+                if (maximizing)
+                    lowestOrHighest = Integer.MAX_VALUE - (100 - depth);
+                else
+                    lowestOrHighest = Integer.MIN_VALUE + (100 - depth);
                 break;
             }
 
@@ -193,87 +163,21 @@ public class Minimax implements AIMoveSelector {
                 break;
             }
 
-            final int currentValue = max(currentBoard, alpha, beta, depth - 1);
-            if (currentValue <= lowest) {
-                lowest = currentValue;
+            final int currentValue = minmax(currentBoard, alpha, beta, depth - 1, !maximizing);
+            if ((!maximizing && currentValue <= lowestOrHighest) || (maximizing && currentValue >= lowestOrHighest)) {
+                lowestOrHighest = currentValue;
             }
 
-            synchronized (alphaLock) {
+            if (!maximizing)
                 alpha = Integer.max(alpha, currentValue);
-                if (alpha > beta) {
-                    break;
-                }
-            }
-        }
-
-        if (board.getVerbose() >= 4) {
-            System.out.print(" ".repeat((6 - depth) * 2));
-            System.out.println(String.format(
-                    "min(): %s's turn, Depth=%d, lowest = %d",
-                    board.getTurn() == 0 ? "White" : "Black",
-                    depth,
-                    lowest));
-        }
-        return lowest;
-    }
-
-    private int max(final Board board, int alpha, int beta, final int depth) {
-        if (depth <= 0) {
-            int value = evaluator.evaluate(board);
-            if (board.getVerbose() >= 4) {
-                System.out.print(" ".repeat((6 - depth) * 2));
-                System.out.println(String.format(
-                        "max(): %s's turn, Depth=%d, evaluate = %d",
-                        board.getTurn() == 0 ? "White" : "Black",
-                        depth,
-                        value));
-            }
-            return value;
-        }
-        int highest = Integer.MIN_VALUE;
-        for (Move m:board.getCurrentPlayerMoves()) {
-            Board currentBoard = new Board(board);
-            currentBoard.executeMove(m);
-            currentBoard.advanceTurn();
-
-            synchronized (processedLock) {
-                ++movesProcessed;
-            }
-
-            Thread.yield();
-
-            // See if that move leaves the other player with no moves and return the lowest value possible if so:
-            if (currentBoard.getCurrentPlayerMoves().size() == 0) {
-                highest = Integer.MAX_VALUE - (100 - depth);
-                break;
-            }
-
-            // If we are past our allowed time then return the highest so far
-            if (maxSeconds > 0 && System.nanoTime() >= stopNanos) {
-                break;
-            }
-
-            final int currentValue = min(currentBoard, alpha, beta, depth - 1);
-            if (currentValue >= highest) {
-                highest = currentValue;
-            }
-
-            synchronized (betaLock) {
+            else
                 beta = Integer.min(beta, currentValue);
-                if (alpha > beta) {
-                    break;
-                }
+
+            if (alpha > beta) {
+                break;
             }
         }
 
-        if (board.getVerbose() >= 4) {
-            System.out.print(" ".repeat((6 - depth) * 2));
-            System.out.println(String.format(
-                    "max(): %s's turn, Depth=%d, highest = %d",
-                    board.getTurn() == 0 ? "White" : "Black",
-                    depth,
-                    highest));
-        }
-        return highest;
+        return lowestOrHighest;
     }
 }
