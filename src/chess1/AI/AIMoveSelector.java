1d0cd810a8f4bd053b8d4e4eae4b0ef50e583413
trent.wyatt <trent@trentwyatt.com>
2019-07-09 19:43:48 -0500

    cruft

--- src/chess1/AI/AIMoveSelector.java
+++ src/chess1/AI/AIMoveSelector.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 import java.util.function.Consumer;
 
 public abstract class AIMoveSelector implements Closeable {
-    int threadPoolSize;
+    final int threadPoolSize;
 
     AIMoveSelector(int maxThreads) {
         threadPoolSize = maxThreads;
