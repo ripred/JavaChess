4020d7f179ea8b6f3c6105af6be00a24f4948268
trent.wyatt <trent@trentwyatt.com>
2019-07-25 18:17:12 -0500

    Removed "game-ending move" flag as it was never fully proven to help

--- src/BestMove.java
+++ src/BestMove.java
@@ -8,33 +8,28 @@ import java.io.Serializable;
 public class BestMove implements Serializable {
     private static final long serialVersionUID = 7218269248347381737L;
 
-    public boolean endGameFound;
     public Move move;
     public int value;
     public int movesExamined;
 
     public BestMove(boolean maximize) {
-        movesExamined = 0;
-        endGameFound = false;
         value = maximize ? LiteUtil.MIN_VALUE : LiteUtil.MAX_VALUE;
+        movesExamined = 0;
         move = null;
     }
 
     @Override
     public String toString() {
-        String string = "Move: ";
-
+        StringBuilder builder = new StringBuilder("Move: ");
         if (move == null) {
-            string += "null";
+            builder.append("null");
         } else {
-            string += String.format("%c%d to %c%d",
-                    move.getFromCol() + 'a',
-                    8 - move.getFromRow(),
-                    move.getToCol() + 'a',
-                    8 - move.getToRow());
+            builder.append(
+            String.format("%c%d to %c%d",
+                    move.getFromCol() + 'a', 8 - move.getFromRow(),
+                    move.getToCol()   + 'a', 8 - move.getToRow()));
         }
-        string += String.format(" Value: %,12d", value);
-
-        return string;
+        builder.append(String.format(" Value: %,12d", value));
+        return builder.toString();
     }
 }
